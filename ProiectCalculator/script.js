e7ab0c8fc48255cac3cc21806151aa7262317fc9
greddy119 <greddy119@bk.ru>
2023-05-30 17:40:15 +0300

    modificari

--- ProiectCalculator/script.js
+++ ProiectCalculator/script.js
@@ -1,121 +1,127 @@
-const cif_0 = document.querySelector('.zero')
-const cif_1 = document.querySelector('.unu')
-const cif_2 = document.querySelector('.doi')
-const cif_3 = document.querySelector('.trei')
-const cif_4 = document.querySelector('.patru')
-const cif_5 = document.querySelector('.cinci')
-const cif_6 = document.querySelector('.sase')
-const cif_7 = document.querySelector('.sapte')
-const cif_8 = document.querySelector('.opt')
-const cif_9 = document.querySelector('.noua')
-const input = document.querySelector('.screen')
-const plus = document.querySelector('.plus');
-const egal = document.querySelector('.egal')
-const stergere = document.querySelector('.stergere')
-const cos = document.querySelector('.cos')
-const sin = document.querySelector('.sin')
-const tan = document.querySelector('.tan')
-const plus_minus = document.querySelector('.plus_minus')
-const put_2 = document.querySelector('.put_2')
-const put_3 = document.querySelector('.put_3')
-const put_10 = document.querySelector('.zece')
-cif_0.addEventListener('click',(ev)=>{
-    input.value += 0;
-})
-cif_1.addEventListener('click',(ev)=>{
-    input.value += 1;
-})
-cif_2.addEventListener('click',(ev)=>{
-    input.value += 2;
-})
-cif_3.addEventListener('click',(ev)=>{
-    input.value += 3;
-})
-cif_4.addEventListener('click',(ev)=>{
-    input.value += 4;
-})
-cif_5.addEventListener('click',(ev)=>{
-    input.value += 5;
-})
-cif_6.addEventListener('click',(ev)=>{
-    input.value += 6;
-})
-cif_7.addEventListener('click',(ev)=>{
-    input.value += 7;
-})
-cif_8.addEventListener('click',(ev)=>{
-    input.value += 8;
-})
-cif_9.addEventListener('click',(ev)=>{
-    input.value += 9;
-})
+let first_number = "0";
+let second_number = "0";
+let result = "0";
+let current_operator;
+let evaluation = [];
+const screen = document.querySelector(".value");
+const keyboard = document.querySelector(".calculatort");
 
+keyboard.addEventListener('click', function(e) {
+    e.stopImmediatePropagation()
+    onButtonPress(e);
+});
 
-window.addEventListener('keydown',(ev)=>{
-    if (ev.keyCode === 8) {
-        let content = input.value;
-        let rez = content.slice(0,-1)
-        console.log(rez)
-        input.value = rez;
+function onButtonPress (e) {
+    switch(e.target.getAttribute('data-button-type')) {
+        case "digit":
+            AssignNumber(e)
+            break;
+        case "operator":
+            AssignOperation(e)
+            break;
     }
-})
+    Render(e);
+}
 
-stergere.addEventListener('click',(ev)=>{
-      input.value = '';
-})
-
-cos.addEventListener('click',(ev)=>{
-        let rez = Math.cos(input.value);
-        input.value =rez;
-})
-
-sin.addEventListener('click',(ev)=>{
-    let rez = Math.sin(input.value);
-    input.value =rez;
-})
-
-tan.addEventListener('click',(ev)=>{
-    let rez = Math.tan(input.value);
-    input.value =rez;
-})
-
-plus_minus.addEventListener('click',(ev)=>{
-    input.value = +input.value * -1;
-})
-
-put_2.addEventListener('click',(ev)=>{
-    input.value = +input.value * +input.value;
-})
-
-put_3.addEventListener('click',(ev)=>{
-    input.value = +input.value * +input.value * +input.value;
-})
-
-put_10.addEventListener('click',(ev)=>{
-    let rez = 1;
-    for (let i = 0; i < input.value; i++) {
-        rez *= 10
+function AssignNumber(e) {
+    if(evaluation.length <= 1) {
+        first_number = first_number === "0"
+            ? e.target.getAttribute("data-value")
+            : first_number + e.target.getAttribute("data-value")
+        if(evaluation.length === 1) evaluation.shift();
+        evaluation.push(first_number)
+        result = first_number;
+        return;
     }
-    input.value = rez;
-})
 
-const comma = () => {
-    let current = input.value;
-    if (!current.includes(".")) {
-        screen.value += ".";
+    if (evaluation.length >= 2) {
+        second_number = second_number === "0"
+            ? e.target.getAttribute("data-value")
+            : second_number + e.target.getAttribute("data-value");
+        if(evaluation.length === 3) evaluation.pop();
+        evaluation.push(second_number);
+        result = second_number;
+
     }
 }
 
+function AssignOperation(e) {
+    current_operator = e.target.getAttribute('data-value');
+    // Exclusive operations that can be performed with one number, in the case of clear it can be executed even when the evaluation array is empty
+    if(current_operator === "%" || current_operator === "+/-" || current_operator === "clear" || current_operator === "=") return Operate();
+    if(evaluation.length === 3) Operate();
+    if(evaluation.length === 2) evaluation.pop();
+    evaluation.splice(1, 1, current_operator);
+}
 
-const equal = (value) => {
-    let current = input.value;
-
-    if (value === "=") {
-        let result = eval(current);
-        input.value = result;
-    } else if (value === "C") {
-        input.value = "";
-    } else {
-        input.value += value;
+function Operate() {
+    if(current_operator === "%" && evaluation.length) {
+        let number = parseInt(evaluation[evaluation.length - 1])
+        result =  (number / 100).toString();
+        evaluation.splice(evaluation.length - 1, 1, result);
+        return;
     }
+
+    if(current_operator === "+/-" && evaluation.length) {
+        result = (evaluation[evaluation.length - 1] * -1).toString();
+        evaluation.splice(evaluation.length - 1, 1, result);
+        return;
+    }
+
+    if(current_operator === "clear") {
+        if(evaluation.length <= 2) {
+            first_number = "0";
+            evaluation = [];
+            result = "0";
+            return;
+        }
+
+        if(evaluation.length === 3) {
+            second_number = "0";
+            evaluation = [first_number.toString()]
+            result = first_number.toString();
+            return;
+        }
+    }
+
+    if(evaluation.length === 3) {
+        result = (eval(evaluation.join().replace(/,/g, ""))).toString();
+        first_number = result;
+        second_number = "0";
+        evaluation = [first_number]
+    }
+}
+
+function Render(e) {
+    const clear_button = document.querySelector('div[data-value="clear"]');
+
+    let new_operator_button = e.target;
+
+    let last_operator_button = document.querySelector('.selected_operation');
+
+    last_operator_button ? last_operator_button.classList.remove('selected_operation') : null;
+    new_operator_button ? new_operator_button.classList.add('selected_operation') : null;
+
+    // change screen's font-size
+    switch(result.toString().length) {
+        case 7:
+            screen.style.fontSize = "4.7rem"
+            break;
+        case 8:
+            screen.style.fontSize = "4.1rem"
+            break;
+        case 9:
+            screen.style.fontSize = "3.65rem"
+            break
+    }
+
+    if(result.toString().length > 9) {
+        screen.textContent = parseFloat(result).toPrecision(3);
+    } else {
+        screen.textContent = result;
+    }
+
+    evaluation.length === "0"
+        ? clear_button.textContent = 'AC'
+        : clear_button.textContent = 'C'
 }
